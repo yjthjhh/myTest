ecf00e26fb24d06bca6b61ae37abaf5f0a2a47fe
yinjitao <573419304@qq.com>
2020-04-21 07:50:37 +0800

    [新增]fastdfsdemo

--- fastdfsdemo/src/main/java/com/example/test/test.java
+++ fastdfsdemo/src/main/java/com/example/test/test.java
@@ -1,9 +1,9 @@
-package com.example;
+package com.example.test;
 
 import org.csource.common.MyException;
 import org.csource.fastdfs.*;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
 
 /**
@@ -13,14 +13,15 @@ public class test {
     public static void main(String[] args) {
         try {
             //设置配置属性
-            Properties properties = new Properties();
+            /*Properties properties = new Properties();
             properties.setProperty("fastdfs.connect_timeout_in_seconds", "10");
             properties.setProperty("fastdfs.network_timeout_in_seconds", "30");
             properties.setProperty("fastdfs.charset", "UTF-8");
             properties.setProperty("fastdfs.http_tracker_http_port", "8080");
-            properties.setProperty("fastdfs.tracker_servers", "192.168.189.128:22122");
+            properties.setProperty("fastdfs.tracker_servers", "192.168.189.128:22122");*/
             //1.通过ClientGlobal类来加载配置文件
-            ClientGlobal.initByProperties(properties);
+            /*ClientGlobal.initByProperties(properties);*/
+            ClientGlobal.init("fastdfsdemo/src/main/java/com/example/test/fastdfs.conf");
             //2.创建TrackerClient对象
             TrackerClient trackerClient = new TrackerClient();
             //3.获取TrackerServer对象
@@ -35,6 +36,14 @@ public class test {
             String path = messages[1];
             String newPath = group + "/" + path;
             System.out.println(newPath);
+            //7.进行文件的下载操作
+            /*byte[] bytes = storageClient.download_file("group1", "M00/00/00/wKi9gF28L0GAWPc8ADBYdST2Acs984.png");
+            FileOutputStream stream = new FileOutputStream("a.png");
+            stream.write(bytes, 0, bytes.length-1);
+            System.out.println("下载成功！！！");*/
+            //8.进行文件的删除操作
+            /*int rows = storageClient.delete_file("group1", "M00/00/00/wKi9gF28L0GAWPc8ADBYdST2Acs984.png");
+            System.out.println(rows);*/
         } catch (IOException e) {
             e.printStackTrace();
         } catch (MyException e) {
